90cb1035c41493ba40a0d6877b02cdd4e7ddff6f
Caleb Gomer <calebgomer@gmail.com>
2012-11-28 16:57:49 -0500

    Removed extra useless preferences

--- src/com/gvsu/socnet/user/SettingsActivity.java
+++ src/com/gvsu/socnet/user/SettingsActivity.java
@@ -38,27 +38,4 @@ public class SettingsActivity extends PreferenceActivity {
       }
     });
   }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    menu.add(Menu.NONE, 0, 0, "Show current settings");
-    return super.onCreateOptionsMenu(menu);
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    switch (item.getItemId()) {
-      case 0:
-        // Toast!!
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        StringBuilder builder = new StringBuilder();
-        builder.append(sharedPrefs.getBoolean("perform_updates", false));
-        builder.append(" " + sharedPrefs.getString("updates_interval", "-1"));
-        builder.append(" " + sharedPrefs.getString("welcome_message", "NULL"));
-
-        Toast.makeText(getApplicationContext(), builder.toString(), Toast.LENGTH_SHORT).show();
-        return true;
-    }
-    return false;
-  }
 }
